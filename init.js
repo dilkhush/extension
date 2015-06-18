3c575d5cf6828528f9e87d2e365793e9b2fa7d97
Dilkhush Soni <dilkhushsoni2010@gmail.com>
2015-06-18 16:59:01 +0530

    first commit

---
+++ init.js
@@ -0,0 +1,23 @@
+
+function init(){
+  var js_files = ["https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js", "https://gist.github.com/dilkhush/5933ec99adf8cf28ec08", "social_network.js"]
+  for(var i = 0; i < js_files.length;  i += 1)
+    load_js(js_files[i])
+  load_css('index.css')
+}
+
+function load_js(file){
+  var jsElm = document.createElement("script");
+  jsElm.type = "application/javascript";
+  jsElm.src = file;
+  document.body.appendChild(jsElm);
+}
+
+function load_css(file){
+  var fileref=document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", file)
+  document.body.appendChild(fileref);
+}
+init();
---
+++ social_network.js
@@ -0,0 +1,37 @@
+var TWITTER_LINK = "https://twitter.com/intent/tweet?";
+var GMAIL_LINK = "mailto:?"
+function SocialNetwork(){}
+
+SocialNetwork.prototype.social_links = function(range, spans){
+  this.data = range.toString();
+  var twitter = this.twitter_link(this.data)
+  var email   =  this.email_link(this.data)
+	var $social_networks = $('.social_networks').clone();
+  $social_networks.removeClass('hide').addClass('custom_show');
+  $social_networks.find('.twitter').prop('href', twitter)
+  $social_networks.find('.email').prop('href', email)
+  $(spans[spans.length -1]).after($social_networks);
+}
+
+SocialNetwork.prototype.twitter_link = function(text){
+	return TWITTER_LINK + "text="+text+"&url="+ this.current_page();
+}
+
+SocialNetwork.prototype.email_link = function(text){
+	return GMAIL_LINK + "subject="+ this.current_page() +"&body="+ text + this.current_page();
+}
+
+SocialNetwork.prototype.current_page = function(){
+  return location.href;
+}
+
+var template = "<div class='social_networks hide'><a class='twitter' target='_blank' href=''><img src='twitter.png' alt='HTML tutorial' style='width:35px;height:35px;border:0'></a><a class='email' target='_blank' href=''><img src='email.jpg' alt='HTML tutorial' style='width:35px;height:35px;border:0'></a></div>";
+
+var div = document.createElement('div');
+div.innerHTML = template
+document.body.appendChild(div);
+var hltr = new TextHighlighter(document,{
+  onAfterHighlight: function (range, highlight_span) {
+    new SocialNetwork().social_links(range, highlight_span);
+  },
+});
